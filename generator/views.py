475eb85f28687e47cc1b612e2489b5d1b865f9d3
bohdandr <bohdandr12@gmail.com>
2023-11-24 00:06:42 +0200

    resume creation template added

--- generator/views.py
+++ generator/views.py
@@ -8,3 +8,17 @@ def index(request):
 def contacts(request):
 	return render(request, "contactUs.html")
 
+def submit_resume(request):
+	full_name = request.POST.get("full_name", "Undefined")
+	date_of_birth = request.POST.get("date_of_birth", "Undefined")
+	hobbies = request.POST.get("hobbies", "None")
+	work_experience = request.POST.getlist("work_experience", [])
+
+	return HttpResponse(f"""
+	        <h2>Full Name: {full_name}</h2>
+	        <div>Date of Birth: {date_of_birth}</div>
+	        <div>Hobbies: {hobbies}</div>
+	        <div>Work Experience: {', '.join(work_experience) if work_experience else "None"}</div>
+	    """)
+
+
